9079ede1f15b3a082190f7c5808054d13f75b3bc
Abdul Hameed Azeemi <abdulhameed.azeemi@hotmail.com>
2021-11-09 02:32:29 +0500

    Update training script

--- examples/textless_nlp/gslm/unit2speech/train_tacotron.py
+++ examples/textless_nlp/gslm/unit2speech/train_tacotron.py
@@ -10,10 +10,40 @@ from examples.textless_nlp.gslm.unit2speech.utils import (
     load_tacotron,
 )
 import torch
-
+from scipy.io.wavfile import read
+import numpy as np
+from examples.textless_nlp.gslm.unit2speech.tacotron2 import layers
 
 from torch import nn
 
+
+class TextMelLoader():
+    """
+        1) loads audio,text pairs
+        2) normalizes text and converts them to sequences of one-hot vectors
+        3) computes mel-spectrograms from audio files.
+    """
+    def __init__(self,  hparams):
+        self.text_cleaners = hparams.text_cleaners
+        self.max_wav_value = hparams.max_wav_value
+        self.sampling_rate = hparams.sampling_rate
+        self.load_mel_from_disk = hparams.load_mel_from_disk
+        self.stft = layers.TacotronSTFT(
+            hparams.filter_length, hparams.hop_length, hparams.win_length,
+            hparams.n_mel_channels, hparams.sampling_rate, hparams.mel_fmin,
+            hparams.mel_fmax)
+
+    def get_mel(self, filename):
+        audio, sampling_rate = load_wav_to_torch(filename)
+        if sampling_rate != self.stft.sampling_rate:
+            raise ValueError("{} {} SR doesn't match target {} SR".format(
+                sampling_rate, self.stft.sampling_rate))
+        audio_norm = audio / self.max_wav_value
+        audio_norm = audio_norm.unsqueeze(0)
+        audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+        melspec = self.stft.mel_spectrogram(audio_norm)
+        melspec = torch.squeeze(melspec, 0)
+        return melspec
 
 class Tacotron2Loss(nn.Module):
     def __init__(self):
@@ -59,6 +89,77 @@ def get_parser():
     return parser
 
 
+def load_wav_to_torch(full_path):
+    sampling_rate, data = read(full_path)
+    return torch.FloatTensor(data.astype(np.float32)), sampling_rate
+
+
+class TextMelCollate():
+    """ Zero-pads model inputs and targets based on number of frames per setep
+    """
+    def __init__(self, n_frames_per_step):
+        self.n_frames_per_step = n_frames_per_step
+
+    def get_mel(self, filename):
+        if not self.load_mel_from_disk:
+            audio, sampling_rate = load_wav_to_torch(filename)
+            if sampling_rate != self.stft.sampling_rate:
+                raise ValueError("{} {} SR doesn't match target {} SR".format(
+                    sampling_rate, self.stft.sampling_rate))
+            audio_norm = audio / self.max_wav_value
+            audio_norm = audio_norm.unsqueeze(0)
+            audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+            melspec = self.stft.mel_spectrogram(audio_norm)
+            melspec = torch.squeeze(melspec, 0)
+        else:
+            melspec = torch.from_numpy(np.load(filename))
+            assert melspec.size(0) == self.stft.n_mel_channels, (
+                'Mel dimension mismatch: given {}, expected {}'.format(
+                    melspec.size(0), self.stft.n_mel_channels))
+
+        return melspec
+
+    def process_batch(self, batch):
+        """Collate's training batch from normalized text and mel-spectrogram
+        PARAMS
+        ------
+        batch: [text_normalized, mel_normalized]
+        """
+        # Right zero-pad all one-hot text sequences to max input length
+        input_lengths, ids_sorted_decreasing = torch.sort(
+            torch.LongTensor([len(x[0]) for x in batch]),
+            dim=0, descending=True)
+        max_input_len = input_lengths[0]
+
+        text_padded = torch.LongTensor(len(batch), max_input_len)
+        text_padded.zero_()
+        for i in range(len(ids_sorted_decreasing)):
+            text = batch[ids_sorted_decreasing[i]][0]
+            text_padded[i, :text.size(0)] = text
+
+        # Right zero-pad mel-spec
+        num_mels = batch[0][1].size(0)
+        max_target_len = max([x[1].size(1) for x in batch])
+        if max_target_len % self.n_frames_per_step != 0:
+            max_target_len += self.n_frames_per_step - max_target_len % self.n_frames_per_step
+            assert max_target_len % self.n_frames_per_step == 0
+
+        # include mel padded and gate padded
+        mel_padded = torch.FloatTensor(len(batch), num_mels, max_target_len)
+        mel_padded.zero_()
+        gate_padded = torch.FloatTensor(len(batch), max_target_len)
+        gate_padded.zero_()
+        output_lengths = torch.LongTensor(len(batch))
+        for i in range(len(ids_sorted_decreasing)):
+            mel = batch[ids_sorted_decreasing[i]][1]
+            mel_padded[i, :, :mel.size(1)] = mel
+            gate_padded[i, mel.size(1)-1:] = 1
+            output_lengths[i] = mel.size(1)
+
+        return text_padded, input_lengths, mel_padded, gate_padded, \
+            output_lengths
+
+
 def main(args, logger):
     # Load quantized audio
     logger.info(f"Loading quantized audio from {args.quantized_unit_path}...")
@@ -84,15 +185,28 @@ def main(args, logger):
     criterion = Tacotron2Loss()
 
     tacotron_dataset = TacotronInputDataset(hparams)
+
+    names_batch, quantized_units_batch = load_quantized_audio_from_file(
+        file_path=quantized_unit_file
+    )
+
+    batch_processor = TextMelCollate(n_frames_per_step=hparams.n_frames_per_step)
+
+    loader = TextMelLoader(hparams)
+
+    batch = []
+    for name, quantized_units in zip(names_batch, quantized_units_batch):
+        quantized_units_str = " ".join(map(str, quantized_units))
+        mel = loader.get_mel(name)
+        batch.append([quantized_units_str, mel])
+
+    training_batch = batch_processor.process_batch(batch)
+
     # ================ MAIN TRAINING LOOP! ===================
     for epoch in range(epoch_offset, hparams.epochs):
         print("Epoch: {}".format(epoch))
 
-        names_batch, quantized_units_batch = load_quantized_audio_from_file(
-            file_path=quantized_unit_file
-        )
-
-        for name, quantized_units in zip(names_batch, quantized_units_batch):
+        for name, quantized_units, training_data in zip(names_batch, quantized_units_batch, training_batch):
 
             start = time.perf_counter()
             for param_group in optimizer.param_groups:
@@ -102,10 +216,13 @@ def main(args, logger):
             tacotron_input = tacotron_dataset.get_tensor(quantized_units_str)
 
             tacotron_model.zero_grad()
-            x, y = tacotron_model.parse_batch(tacotron_input)
-            y_pred = tacotron_model(x)
 
-            loss = criterion(y_pred, y)
+            _, _, mel_padded, gate_padded, _ = training_data
+            y = [mel_padded, gate_padded]
+
+            model_output = tacotron_model.inference(tacotron_input, None, ret_has_eos=False)
+
+            loss = criterion(model_output, y)
 
             reduced_loss = loss.item()
             loss.backward()
